d410693d7369c319ede64a6f158abefe6b5e7ba3
rania333 <raniaaramadan1999@gmail.com>
2022-09-30 00:54:33 +0200

    refactor routes

--- index.js
+++ index.js
@@ -4,22 +4,13 @@ const dotenv = require('dotenv')
 dotenv.config({ path: 'config.env' }) // lazem l step de l2n asm l file not .env
 const express = require('express')
 //routes
-const categoryRoutes = require('./routes/categoryRoute')
-const subCategoryRoutes = require('./routes/subCategoryRoute')
-const brandRoutes = require('./routes/brandRoutes')
-const productRoutes = require('./routes/productRoutes')
-const userRoutes = require('./routes/userRoutes')
-const authRoutes = require('./routes/authRoutes')
-const reviewRoutes = require('./routes/reviewRoutes')
-const wishRoutes = require('./routes/wishlistRoutes')
-const addressRoutes = require('./routes/addressRoute')
 
 //global err
-const { ErrorHandler } = require('./utils/Error')
 const { ErrorMiddleware } = require('./middlewares/errMiddleware')
 
 //db connection
 const dbConnection = require('./config/database')
+const { mountRoute } = require('./routes')
 dbConnection()
 
 const app = express()
@@ -28,22 +19,8 @@ const app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'uploads'))) //mw to serve file
 
-// route 
-app.use('/category', categoryRoutes)
-app.use('/subcategory', subCategoryRoutes)
-app.use('/brand', brandRoutes)
-app.use('/product', productRoutes)
-app.use('/user', userRoutes)
-app.use('/auth', authRoutes)
-app.use('/review', reviewRoutes)
-app.use('/wishlist', wishRoutes)
-app.use('/address', addressRoutes)
-
-app.all('*', (req, res, nxt) => {
-    const err = new ErrorHandler("can't find this route", 500)
-    nxt(err) //bb3t l err ll middleware
-})
-
+// mount route 
+mountRoute(app)
 //err handler
 app.use(ErrorMiddleware)
 

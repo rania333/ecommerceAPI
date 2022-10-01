013dcb5ba548f749632b4caf5d303a7b229aee00
rania333 <raniaaramadan1999@gmail.com>
2022-10-01 02:04:49 +0200

    limit requests within time

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config({ path: 'config.env' }) // lazem l step de l2n asm l file not .env
 const express = require('express')
 const cors = require('cors');
 const compression = require('compression');
+const rateLimiter = require('express-rate-limit')
 //routes
 
 //global err
@@ -32,6 +33,18 @@ app.post(
 //middlewares
 app.use(express.json({ limit: '20mb' }))
 app.use(express.static(path.join(__dirname, 'uploads'))) //mw to serve file
+
+// Limit each IP to 100 requests per `window` (here, per 15 minutes)
+const limiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+    message:
+        'Too many accounts created from this IP, please try again after an hour',
+});
+
+// Apply the rate limiting middleware to all requests
+app.use('/', limiter);
+
 
 // mount route 
 mountRoute(app)
